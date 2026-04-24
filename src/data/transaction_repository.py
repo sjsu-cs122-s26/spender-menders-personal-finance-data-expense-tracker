4da222b539eef4875731fd471a2225520f186865
Sol_P_T <91825053+ssolh2906@users.noreply.github.com>
2026-04-24 16:16:17 -0700

    Add query functions

---
+++ src/data/transaction_repository.py
@@ -0,0 +1,57 @@
+from models import Transaction
+
+
+class TransactionRepository:
+    def __init__(self, session):
+        self.session = session
+
+    def add(self, expense_cat_id, amount, date, description=""):
+        tx = Transaction(
+            expense_cat_id=expense_cat_id,
+            amount=amount,
+            date=date,
+            description=description,
+        )
+        self.session.add(tx)
+        self.session.commit()
+        return tx
+
+    def get_by_id(self, tx_id):
+        return self.session.query(Transaction).filter_by(transactions_id=tx_id).first()
+
+    def get_all(self):
+        return self.session.query(Transaction).all()
+
+    def get_by_category(self, cat_id):
+        return self.session.query(Transaction).filter_by(expense_cat_id=cat_id).all()
+
+    def get_by_date_range(self, start, end):
+        return (
+            self.session.query(Transaction)
+            .filter(Transaction.date >= start)
+            .filter(Transaction.date <= end)
+            .all()
+        )
+
+    def update(self, tx_id, amount=None, date=None, description=None, expense_cat_id=None):
+        tx = self.get_by_id(tx_id)
+        if tx is None:
+            return None
+        if amount is not None:
+            tx.amount = amount
+        if date is not None:
+            tx.date = date
+        if description is not None:
+            tx.description = description
+        if expense_cat_id is not None:
+            tx.expense_cat_id = expense_cat_id
+        self.session.commit()
+        return tx
+
+    def delete(self, tx_id):
+        tx = self.get_by_id(tx_id)
+        if tx is None:
+            return False
+        self.session.delete(tx)
+        self.session.commit()
+        return True
