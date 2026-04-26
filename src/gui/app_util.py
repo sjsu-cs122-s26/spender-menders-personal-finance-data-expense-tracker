4cc3a3ae1747aa4478718427c76aa41cbb4ad912
Daphne Dao <daphnedao19@gmail.com>
2026-04-25 22:22:10 -0700

    account create/delete/update functional

--- src/gui/app_util.py
+++ src/gui/app_util.py
@@ -4,6 +4,7 @@
 
 ACCOUNT_NAME: str = 'Test2'
 ACCOUNT_BAL:  int = 150
+ACCOUNT_ID:   int = 1
 
 # ------------------------------------------------------------------------------------------------ #
 #                                             GUI STYLE                                            #
@@ -12,3 +13,20 @@ ACCOUNT_BAL:  int = 150
 GUI_TITLE: str = 'Expense Tracker'
 QLABEL_STYLE: str = " QLabel {background-color: #262626; border: none; color: white; }"
 QLIST_STYLE: str = " QListWidget {background-color: #262626; color: white; }"
+QFRAME_STYLE: str = """
+                          QFrame {
+                              background-color: #2b2b2b;
+                          }
+                          QLabel {
+                              color: white;
+                          }
+                          QLineEdit {
+                              background-color: #3a3a3a;
+                              color: white;
+                              border: 1px solid #555;
+                          }
+                      """
+
+CREATE_BTTN: str = "Create"
+DELETE_BTTN: str = "Delete"
+UPDATE_BTTN: str = "Update"
