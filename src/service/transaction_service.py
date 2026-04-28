a665caa7186eeacfce91e1c96ab68b498aa89b3f
Heather Ho <heatherho@gmail.com>
2026-04-28 11:57:46 -0700

    Update balance with transaction

--- src/service/transaction_service.py
+++ src/service/transaction_service.py
@@ -1,15 +1,26 @@
 from src.data.database import DatabaseManager
 from src.data.transaction_repository import TransactionRepository
+from src.service.category_service import CategoryService
+from src.service.account_service import AccountService
 
 class TransactionService:
     def __init__(self):
         db = DatabaseManager()
         db.init_db()
         self.repo = TransactionRepository(db.get_session())
+        self.category_service = CategoryService()
+        self.account_service = AccountService()
     
-    def add_transaction(self, cat_id, amount, date, description=""):
-        return self.repo.add(cat_id, amount, date, description)
-    
+    def add_transaction(self, account_id, cat_id, amount, date, description=""): # update balance after transcation
+        category = self.category_service.get_category(cat_id) # check if category exists
+        if category:
+            signed_amount = self.category_service.get_signed_amount(cat_id, amount) # get signed amount based on category type
+        new_balance = self.account_service.get_account_balance(account_id) + signed_amount # calculate new balance
+        self.account_service.update_account(account_id, balance=new_balance) # update account balance
+        tx = self.repo.add(account_id, cat_id, amount, date, description) # add transaction
+
+        return tx
+
     def get_transaction(self, tx_id):
         if tx_id is None:
             return self.repo.get_all()
