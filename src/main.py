d19b857da958778ca484c5736e74b0c77aff7bb3
allysonmw <allyson.wong@sjsu.edu>
2026-05-01 19:00:54 -0700

    resolved merge conflicts, keeping other changes since mine isn't working for some reason

--- src/main.py
+++ src/main.py
@@ -1,17 +1,6 @@
-import sys
-from pathlib import Path
-
-_PROJECT_ROOT = Path(__file__).resolve().parent.parent
-if str(_PROJECT_ROOT) not in sys.path:
-    sys.path.insert(0, str(_PROJECT_ROOT))
-
-import tkinter as tk
-from src.gui.app import ExpenseTrackerApp
-
 def main():
-    root = tk.Tk()
-    app = ExpenseTrackerApp(root)
-    root.mainloop()
+    # Initiate GUI and start the application here
+    pass
 
 if __name__ == "__main__":
     main()
