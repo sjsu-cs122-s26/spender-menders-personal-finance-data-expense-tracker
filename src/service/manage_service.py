2bd89742a5ccc35b874ca8d013846df8622e6523
Sol_P_T <91825053+ssolh2906@users.noreply.github.com>
2026-05-02 15:34:59 -0700

    Migrate business logics, Management -> manage service

--- src/service/manage_service.py
+++ src/service/manage_service.py
@@ -1,4 +1,16 @@
+from src.data.df_utils import orm_to_df
+from src.data.models import Account, Category, Transaction
+from src.service.account_service import AccountService
+from src.service.category_service import CategoryService
+from src.service.transaction_service import TransactionService
+
+
 class ManageService:
+    def __init__(self):
+        self.account_service = AccountService()
+        self.category_service = CategoryService()
+        self.transaction_service = TransactionService()
+
     def get_expense_sum(self, account_id):
         self._refresh()
         df = self._merge_df
@@ -7,7 +19,11 @@ class ManageService:
         )
 
     def _refresh(self):
-        pass
+        cats = self.category_service.get_all_categories()
+        accs = self.account_service.get_all_accounts()
+        trans = self.transaction_service.get_all_transactions()
 
-    def _merge_df(self, df):
-        pass
+        self._cat_df = orm_to_df(cats, Category)
+        self._acc_df = orm_to_df(accs, Account)
+        self._trans_df = orm_to_df(trans, Transaction)
+        self._merge_df = self._trans_df.merge(self._cat_df, on="cat_id")
