2377a17026d3c241fbe5377fdd826473f363fc03
Heather Ho <heatherho@gmail.com>
2026-05-02 23:56:26 -0700

    update visualization service

--- src/service/visualization_service.py
+++ src/service/visualization_service.py
@@ -1,8 +1,9 @@
-from src.data.database import DatabaseManager
+from src.data.df_utils import orm_to_df
+from src.data.df_utils import convert_date_to_month
+from src.data.models import Transaction
 from src.service.category_service import CategoryService
 from src.service.account_service import AccountService
 from src.service.transaction_service import TransactionService
-#TODO: add function to database manager to join transactions with categories and accounts for easier data retrieval
 
 class VisualizationService: 
     '''Service to prepare data for visualization, such as charts and graphs.'''
@@ -10,6 +11,12 @@ class VisualizationService:
         self.account_service = AccountService()
         self.category_service = CategoryService()
         self.transaction_service = TransactionService()
+
+    def get_transactions_merged_with_categories_df(self, account_id):
+        df = orm_to_df(self.transaction_service.get_merged_transactions_with_categories(), Transaction)
+        df['month'] = df['date'].apply(convert_date_to_month)
+        return df
+    
     def get_monthly_expenses_by_category(self, month):
         pass
     def get_monthly_income_by_category(self, month):
